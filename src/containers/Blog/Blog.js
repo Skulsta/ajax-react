65f8f41d5df7b51d573035d6639706e468c6b73a
skulsta <sti.sku@gmail.com>
2020-06-22 08:41:34 +0200

    Added error handling when fetching posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -10,36 +10,45 @@ class Blog extends Component {
   state = {
     posts: [],
     postSelected: null,
+    error: false,
   };
   componentDidMount() {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
-      const posts = response.data.slice(0, 4);
-      const updatedPosts = posts.map((post) => {
-        return {
-          ...post,
-          author: "Stian",
-        };
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map((post) => {
+          return {
+            ...post,
+            author: "Stian",
+          };
+        });
+        this.setState({ posts: updatedPosts });
+        // console.log(response);
+      })
+      .catch((error) => {
+        // console.log(error);
+        this.setState({ error: true });
       });
-      this.setState({ posts: updatedPosts });
-      // console.log(response);
-    });
   }
 
   postSelectedHandler = (id) => {
     this.setState({ postSelected: id });
   };
   render() {
-    const posts = this.state.posts.map((post) => {
-      return (
-        <Post
-          key={post.id}
-          title={post.title}
-          author={post.author}
-          clicked={() => this.postSelectedHandler(post.id)}
-        />
-      );
-    });
-
+    let posts = <p style={{ color: "red" }}>Something went wrong</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map((post) => {
+        return (
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
+        );
+      });
+    }
     return (
       <div>
         <section className="Posts">{posts}</section>
